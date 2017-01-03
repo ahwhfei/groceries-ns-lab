368ea180f3d2883f52ed6562158296ad60e9d458
Hongfei Wu <hongfei.wu@citrix.com>
2017-01-03 17:25:09 +0800

    Add router to list page

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,48 +1,8 @@
 import { Component } from "@angular/core";
-import { User } from "./shared/user/user";
-
-import { UserService } from "./shared/user/user.service";
 
 @Component({
-    selector: "my-app",
-    providers: [UserService],
-    templateUrl: "pages/login/login.html",
-    styleUrls: ["pages/login/login-common.css", "pages/login/login.css"]
+    selector: "main",
+    template: "<page-router-outlet></page-router-outlet>"
 })
-export class AppComponent {
-    user: User;
-    isLoggingIn = true;
 
-    constructor(private userService: UserService) {
-        this.user = new User();
-        this.user.email = "408869502@qq.com";
-        this.user.password = "123456";
-    }
-
-    submit() {
-        if (this.isLoggingIn) {
-            this.login();
-        } else {
-            this.signUp();
-        }
-    }
-
-    login() {
-
-    }
-
-    signUp() {
-        this.userService.register(this.user)
-            .subscribe(
-                () => {
-                    alert("Your account was successfully created.");
-                    this.toggleDisplay();
-                },
-                () => alert("Unfortunately we were unable to create your account.")
-            );
-    }
-    
-    toggleDisplay() {
-        this.isLoggingIn = !this.isLoggingIn;
-    }
-}
+export class AppComponent { }
--- app/app.module.ts
+++ app/app.module.ts
@@ -2,16 +2,23 @@ import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptFormsModule } from "nativescript-angular/forms";
 import { NativeScriptHttpModule } from "nativescript-angular/http";
 import { NativeScriptModule } from "nativescript-angular/platform";
+import { NativeScriptRouterModule } from "nativescript-angular/router";
 
 import { AppComponent } from "./app.component";
+import { routes, navigatableComponents } from "./app.routing";
 
 @NgModule({
-    declarations: [AppComponent],
+    declarations: [
+        AppComponent,
+        ...navigatableComponents
+    ],
     bootstrap: [AppComponent],
     imports: [
         NativeScriptModule, 
         NativeScriptFormsModule,
-        NativeScriptHttpModule
+        NativeScriptHttpModule,
+        NativeScriptRouterModule,
+        NativeScriptRouterModule.forRoot(routes)
     ],
     schemas: [NO_ERRORS_SCHEMA]
 })
